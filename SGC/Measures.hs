6fb8fe0fd88f8ff9216f71a8f9e50f268c8920a3
fehu <lord.fehu@gmail.com>
2015-11-10 17:51:36 -0600

    implemented WithUnitSystem monad; added UnitsDivide class;

--- SGC/Measures.hs
+++ SGC/Measures.hs
@@ -42,39 +42,44 @@ module SGC.Measures (
 , UnitFor(..)
 
 -- * Units Operations
-, UnitsMultiply (uMultiply)
---, uMultiply
---, uDivide
---, ResultDim(..)
+, uMultiply
+, uDivide
+--, UnitsMultiply (uMultiply, (~*~))
+--, UnitsDivide   (uDivide,   (~/~))
 
----- * Measured Values
---, Measured(..)
-----, measure
---, UnitEv
---
----- * 'WithUnitSystem' Monad.
---, WithUnitSystem
---, withUnitSystem
+-- * 'WithUnitSystem' Monad
+, WithUnitSystem
+, withUnitSystem
+
+, mkScalar
+, mkVector
+
+, (~*~)
+, (~/~)
 
 
 ) where
 
+import Control.Applicative
 
 -----------------------------------------------------------------------------
 
-class (Show q) => PhysicalQuantity q where --isScalar :: q -> Bool
-                                           --isVector :: q -> Bool
+class (Show q) => PhysicalQuantity q where quantityId :: q
 
-                                           quantityId :: q
-
-                                           --isVector = not . isScalar
-                                           --isScalar = not . isVector
 
 data QuantityDimensions = Dimensionless
                         | UScalar
                         | UVector
 
 type family PhQDim q :: QuantityDimensions
+
+
+-----------------------------------------------------------------------------
+
+class VectorOps v n where vMultiplyConst' :: v n -> n -> v n
+                          vDivideConst'   :: v n -> n -> v n
+
+-----------------------------------------------------------------------------
 
 
 data Time     = Time        deriving Show
@@ -85,6 +90,8 @@ data AbsSpeed = AbsSpeed    deriving Show
 data Position = Position    deriving Show
 data Speed    = Speed       deriving Show
 
+
+-----------------------------------------------------------------------------
 
 
 instance PhysicalQuantity Time      where quantityId = Time
@@ -126,9 +133,11 @@ qDivide _ _ = quantityId
 
 type instance PhQDiv Position Time = Speed
 type instance PhQMult Speed Time = Position
+type instance PhQMult Time Speed = Position
 
 type instance PhQDiv Distance Time = AbsSpeed
 type instance PhQMult AbsSpeed Time = Distance
+type instance PhQMult Time AbsSpeed = Distance
 
 -----------------------------------------------------------------------------
 
@@ -153,12 +162,8 @@ class (Fractional n, Unit u s n) =>
         scalarUnit _ _ = createUnit
 
 
---type family IsVector (v :: * -> *) :: Bool
-
---type family NumVector (v :: *) :: *
-
 -- |
-class (Fractional n, Unit u s (v n), VectorOps v n) => -- IsVector v ~ True,
+class (Fractional n, Unit u s (v n), VectorOps v n) =>
 
     VectorUnit u s v n where
 
@@ -200,17 +205,18 @@ type UnitOpsConstraints s a b c qa qb qc da db dc na nb nc = (
      )
 
 
---class UnitsMultiply s na da nb db where -- nc dc
---    uMultiply :: ( PhQMult qa qb ~ qc
---                 , UnitOpsConstraints s a b c qa qb qc da db dc na nb nc) =>
---          s -> a na -> b nb -> c nc
+-----------------------------------------------------------------------------
 
-
-
-class UnitsMultiply s na da nb db nc where -- nc dc
+class UnitsMultiply s na da nb db nc where
     uMultiply :: ( PhQMult qa qb ~ qc
                  , UnitOpsConstraints s a b c qa qb qc da db dc na nb nc) =>
           s -> a na -> b nb -> c nc
+
+    (~*~) :: ( PhQMult qa qb ~ qc
+             , UnitOpsConstraints s a b c qa qb qc da db dc na nb nc) =>
+          a na -> b nb -> WithUnitSystem s (c nc)
+    (~*~) x y = WithUnitSystem $ \s -> uMultiply s x y
+
 
 
 instance
@@ -218,16 +224,83 @@ instance
         uMultiply _ x y = createUnit $ unitValue x * unitValue y
 
 
-class VectorOps v n where vMultiplyConst' :: v n -> n -> v n
-
 instance (VectorOps v n) =>
     UnitsMultiply s (v n) UVector n UScalar (v n) where
         uMultiply _ x y = createUnit $ vMultiplyConst' (unitValue x)
                                                        (unitValue y)
 
+instance (VectorOps v n) =>
+    UnitsMultiply s n UScalar (v n) UVector (v n) where
+        uMultiply _ x y = createUnit $ vMultiplyConst' (unitValue y)
+                                                       (unitValue x)
+
+-----------------------------------------------------------------------------
+
+class UnitsDivide s na da nb db nc where
+    uDivide :: ( PhQDiv qa qb ~ qc
+               , UnitOpsConstraints s a b c qa qb qc da db dc na nb nc) =>
+          s -> a na -> b nb -> c nc
+
+    (~/~) :: ( PhQDiv qa qb ~ qc
+               , UnitOpsConstraints s a b c qa qb qc da db dc na nb nc) =>
+          a na -> b nb -> WithUnitSystem s (c nc)
+    (~/~) x y = WithUnitSystem $ \s -> uDivide s x y
+
+instance (Fractional n) =>
+    UnitsDivide s n UScalar n UScalar n where
+        uDivide _ x y = createUnit $ unitValue x / unitValue y
+
+instance (VectorOps v n) =>
+    UnitsDivide s (v n) UVector n UScalar (v n) where
+        uDivide _ x y = createUnit $ vDivideConst' (unitValue x)
+                                                   (unitValue y)
+
+instance (VectorOps v n) =>
+    UnitsDivide s n UScalar (v n) UVector (v n) where
+        uDivide _ x y = createUnit $ vDivideConst' (unitValue y)
+                                                   (unitValue x)
+
+
+-----------------------------------------------------------------------------
+
+mkScalar q n = WithUnitSystem $ \s -> scalarUnit s q n
+mkVector q n = WithUnitSystem $ \s -> vectorUnit s q n
+
+-----------------------------------------------------------------------------
+
+newtype WithUnitSystem s a = WithUnitSystem (s -> a)
+
+instance (UnitSystem s) => Functor (WithUnitSystem s) where
+    fmap f (WithUnitSystem g) = WithUnitSystem $ f . g
+
+instance (UnitSystem s) => Applicative (WithUnitSystem s) where
+    pure = WithUnitSystem . const
+    WithUnitSystem f <*> WithUnitSystem g = WithUnitSystem (\s -> f s $ g s)
+
+instance (UnitSystem s) => Monad (WithUnitSystem s) where
+    return = pure
+    WithUnitSystem g >>= f = WithUnitSystem $ \s ->
+                                let WithUnitSystem f' = f (g s)
+                                in f' s
+
+mapwus2 f x y = do x' <- x
+                   y' <- y
+                   return $ f x' y'
+
+instance (Num a, UnitSystem s) => Num (WithUnitSystem s a) where
+    (+) = mapwus2 (+)
+    (*) = mapwus2 (*)
+    abs = fmap abs
+    signum = fmap signum
+    negate = fmap negate
+    fromInteger = pure . fromInteger
+
+withUnitSystem :: (UnitSystem s) => s -> WithUnitSystem s a -> a
+withUnitSystem s (WithUnitSystem g) = g s
 
 -----------------------------------------------------------------------------
 -- TODO: Tests
+
 
 absSpeed s = scalarUnit s AbsSpeed (5 :: Float)
 time s = scalarUnit s Time (10 :: Float)
@@ -240,6 +313,7 @@ data Vec2 n = Vec2 n n deriving Show
 instance Num (Vec2 n)
 instance (Fractional n) =>
     VectorOps Vec2 n where vMultiplyConst' (Vec2 x y) c = Vec2 (c*x) (c*y)
+                           vDivideConst'   (Vec2 x y) c = Vec2 (x/c) (y/c)
 
 --instance Num (Vec2 n)
 
@@ -247,4 +321,12 @@ speed s = vectorUnit s Speed (Vec2 2.3 5.2 :: Vec2 Float)
 dist2 s = uMultiply s (speed s) (time s)
 
 
+absSpeed' s = uDivide s (dist s) (time s)
+speed' s = uDivide s (dist2 s) (time s)
+
+
+foo s = withUnitSystem s $ do
+          v <- mkVector Speed (Vec2 1 (-1) :: Vec2 Float)
+          t <- mkScalar Time (5 :: Float)
+          v ~*~ t
 
