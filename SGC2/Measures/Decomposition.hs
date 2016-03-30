a35a9ea12908a1e7b055e1f1871360ac95cfd509
fehu <lord.fehu@gmail.com>
2016-03-30 14:30:31 -0600

    forall (Unit A, Unit B) => A * B == B * A

--- SGC2/Measures/Decomposition.hs
+++ SGC2/Measures/Decomposition.hs
@@ -17,7 +17,8 @@
            , TypeOperators
            , UndecidableInstances
            , Rank2Types
---           , PolyKinds
+           , PolyKinds
+           , FunctionalDependencies
        #-}
 
 module SGC2.Measures.Decomposition where
@@ -28,49 +29,18 @@ import Data.Typeable
 import Data.Type.Equality
 import Data.Type.Bool
 
+import GHC.TypeLits (Nat, CmpNat)
 import TypeNum.Integer
 import TypeNum.Rational
 
------------------------------------------------------------------------------
-
---newtype (Unit u) => AtomWeight u (i :: TInt) = AtomWeight u
-
-newtype (Unit u) => AtomWeight u (i :: TInt) = AtomWeight u
-
-data P a b = P a b
-
-atomUnit (AtomWeight u) = u
-
-atomWeight' :: AtomWeight a i -> Int' i
-atomWeight' _ = Int'
-
-atomWeight :: (TIntValue i) => AtomWeight a i -> Integer
-atomWeight = intValue . atomWeight'
-
 
 -----------------------------------------------------------------------------
 
-class (Unit u) => UnitDecomposition u where type UnitStructure' u :: [(*, TInt)]
+class (Unit u) => UnitDecomposition u where type UnitStructure u :: [(*, TInt)]
+                                            type UnitHash      u :: Nat
 
 
 -----------------------------------------------------------------------------
-
----- map types list
---type family (<$>) (f :: * -> *) (l :: [*]) :: [*] where
---    f <$> '[]      = '[]
---    f <$> (h ': t) = (f h) ': (f <$> t)
-
-
---type family (++) (l1 :: [*]) (l2 :: [*]) :: [*] where
---    '[]      ++ l = l
---    (h ': t) ++ l = h ': (t ++ l)
-
-
----- map types list
---type family (<$>) (f :: * -> (*, TInt)) (l :: [*]) :: [(*, TInt)] where
---    f <$> '[]      = '[]
---    f <$> (h ': t) = (f h) ': (f <$> t)
-
 
 type family (++) (l1 :: [(*, TInt)]) (l2 :: [(*, TInt)]) :: [(*, TInt)] where
     '[]      ++ l = l
@@ -81,31 +51,9 @@ type family NegatePowers (l :: [(*, TInt)]) :: [(*, TInt)] where
     NegatePowers '[] = '[]
     NegatePowers ('(u, x) ': t) = '(u, Negate x) ': NegatePowers t
 
------------------------------------------------------------------------------
-
-type family UnitWeight (l :: [(*, TInt)]) (u :: *) :: TInt where
-    UnitWeight '[]             u = Zero
-    UnitWeight ('(u', i) ': t) u = If (u == u') i (UnitWeight t u)
-
-
-type family Contains (k :: *) (l :: [*]) :: Bool where
-    Contains k '[]            = False
-    Contains k (k' ': t) = k == k' || Contains k t
-
-
-type family ContainsKey (k :: *) (l ::[(*, TInt)]) :: Bool where
-    ContainsKey k '[]            = False
-    ContainsKey k ('(u, i) ': t) = k == u || ContainsKey k t
-
-
-
-type family UniqueKeys' (l   :: [(*, TInt)]) (acc :: [*]) :: [*] where
-    UniqueKeys' '[] acc            = acc
-    UniqueKeys' ('(u, i) ': t) acc = UniqueKeys' t (If (u `Contains` acc) acc (u ': acc))
-
-type UniqueKeys (l :: [(*, TInt)]) = UniqueKeys' l '[]
 
 -----------------------------------------------------------------------------
+
 
 type family DecompositionEq (a :: [(*, TInt)]) (b :: [(*, TInt)]) :: Bool where
     DecompositionEq '[] '[] = True
@@ -113,50 +61,40 @@ type family DecompositionEq (a :: [(*, TInt)]) (b :: [(*, TInt)]) :: Bool where
                                                           ia == ib &&
                                                           DecompositionEq ta tb
 
+-----------------------------------------------------------------------------
+
+type family Compare a b :: Ordering where
+    Compare a b = CmpNat (UnitHash a) (UnitHash b)
 
 
-
-type family DecompositionSum (as :: [(*, TInt)]) (bs :: [(*, TInt)]) :: [(*, TInt)] where
-    DecompositionSum as bs = DecompositionSum' (UniqueKeys (as ++ bs)) as bs
-
-
-type family DecompositionSum' ks as bs where
-    DecompositionSum' '[] as bs      = '[]
-    DecompositionSum' (k ': t) as bs = '(k, UnitWeight as k + UnitWeight bs k)
-                                    ': DecompositionSum' t as bs
+type family FilterOrd' (ord :: Ordering) (e :: k) (l :: [(k, TInt)]) :: [(k, TInt)] where
+    FilterOrd' ord e '[] = '[]
+    FilterOrd' ord e ('(u, n) ': t) = If (Compare e u == ord ) ('(u, n) ': FilterOrd' ord e t)
+                                                               (FilterOrd' ord e t)
 
 
+type family TQSort' (l :: [(*, TInt)]) :: [(*, TInt)] where
+    TQSort' '[] = '[]
+    TQSort' ('(u, n) ': t) = TQSort' (FilterOrd' LT u t)
+                          ++ '[ '(u, n) ]
+                          ++ TQSort' (FilterOrd' GT u t)
+
+type family DecompositionEq' (a :: [(*, TInt)]) (b :: [(*, TInt)]) :: Bool where
+    DecompositionEq' '[] '[] = True
+    DecompositionEq' as bs   = DecompositionEq (TQSort' as) (TQSort' bs)
 
 -----------------------------------------------------------------------------
 
-
-type (!==) a b = DecompositionEq (UnitStructure' a) (UnitStructure' b) ~ True
-
-
---type family (!*) a b where
-
-
---type family (===) a b where
---    a === b = DecompositionEq (UnitStructure' a) (UnitStructure' b)
-
---class UnitEq a b where a === b
-
-
------------------------------------------------------------------------------
-
-data AtomsWeights (l :: [(*, TInt)]) where
-    WeightsNil  :: AtomsWeights '[]
-    WeightsCons :: AtomWeight u i -> AtomsWeights l -> AtomsWeights ('(u,i) ': l)
-
-type UnitStructure u = AtomsWeights (UnitStructure' u)
+type (!==) a b = DecompositionEq' (UnitStructure a) (UnitStructure b) ~ True
 
 -----------------------------------------------------------------------------
 
 instance (Unit a, Unit b) => UnitDecomposition (a * b) where
-    type UnitStructure' (a * b) = UnitStructure' a  ++ UnitStructure' b
+    type UnitStructure (a * b) = UnitStructure a  ++ UnitStructure b
 instance (Unit a, Unit b) => UnitDecomposition (a / b) where
-    type UnitStructure' (a / b) = UnitStructure' a  ++ NegatePowers (UnitStructure' b)
---instance (Unit a, AsRational p,  IntValuesForRational (AsRational' p)) => UnitDecomposition (a ^ p) where
+    type UnitStructure (a / b) = UnitStructure a  ++ NegatePowers (UnitStructure b)
+--instance (Unit a, AsRational p,  IntValuesForRational (AsRational' p)) => UnitDecomposition (a ^ p)
+--    where type UnitStructure' (a ^ p) = UnitStructure' a
 
 -----------------------------------------------------------------------------
 
--- SGC2/Measures/Units.hs
+++ SGC2/Measures/Units.hs
@@ -32,7 +32,8 @@ data Second = Second
 instance Unit Second where unitName _ = "s"
                            unitInstance = Second
 instance UnitDecomposition Second where
-    type UnitStructure' Second = '[ '(Second, Pos 1) ]
+    type UnitStructure Second = '[ '(Second, Pos 1) ]
+    type UnitHash Second = 0
 
 
 
@@ -40,7 +41,17 @@ data Meter = Meter
 instance Unit Meter where unitName _ = "m"
                           unitInstance = Meter
 instance UnitDecomposition Meter where
-    type UnitStructure' Meter = '[ '(Meter, Pos 1) ]
+    type UnitStructure Meter = '[ '(Meter, Pos 1) ]
+    type UnitHash Meter = 1
+
+
+data Kilogramm = Kilogramm
+instance Unit Kilogramm where unitName _ = "kg"
+                              unitInstance = Kilogramm
+instance UnitDecomposition Kilogramm where
+    type UnitStructure Kilogramm = '[ '(Kilogramm, Pos 1) ]
+    type UnitHash Kilogramm = 2
+
 
 
 
@@ -48,11 +59,14 @@ data MeterPerSecond = MeterPerSecond
 instance Unit MeterPerSecond where unitName _ = "m/s"
                                    unitInstance = MeterPerSecond
 instance UnitDecomposition MeterPerSecond where
-    type UnitStructure' MeterPerSecond = '[ '(Meter, Pos 1) , '(Second, Neg 1) ]
+    type UnitStructure MeterPerSecond = '[ '(Meter, Pos 1) , '(Second, Neg 1) ]
 
 
-
-
+data Newton = Newton
+instance Unit Newton where unitName _ = "N"
+                           unitInstance = Newton
+instance UnitDecomposition Newton where
+    type UnitStructure Newton = UnitStructure (Kilogramm * Meter / (Second ^ Int' (Pos 2)))
 
 
 
@@ -67,12 +81,43 @@ x :: (Second !== x) => x
 x = undefined
 
 
-xx :: (MeterPerSecond !== x) => x -> Int
-xx = undefined
+--xx :: (Newton !== x) => x -> Int
+--xx = undefined
 
 aa :: Meter / Second
 aa = undefined
 
-xx' = xx aa -- (Meter :/ Second)
+--aa' :: Kilogramm * Meter / (Second ^ Int' (Pos 2)) -- / Second / Second
+--aa' = undefined
+
+--xx' = xx aa' -- (Meter :/ Second)
+
+
+-----------------------------------------------------------------------------
+
+data AA = AA
+instance Unit AA where unitName _ = "A"
+                       unitInstance = AA
+instance UnitDecomposition AA where type UnitStructure AA = UnitStructure (Meter * Second)
+
+
+qq :: (AA !== x) => x -> Int
+qq = undefined
+
+qq' = qq (Second :* Meter)
+
+qq'' = qq (Meter :* Second)
+
+--qq''' = qq (Meter :* Kilogramm)
+
+-----------------------------------------------------------------------------
+
+
+ww :: ((Meter * Second) !== (Second * Meter)) => x
+ww = undefined
+--
+--ww' :: ((Meter * Second) !== (Second * Kilogramm)) => x
+--ww' = undefined
+
 
 
